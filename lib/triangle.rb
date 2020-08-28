c4ab803604415666c8eff95ae056e075506a5679
Rebecca Congi <rncongi@gmail.com>
2020-08-28 21:08:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_reader :length_1, :length_2, :length_3
+  
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+    
+  end 
+  
+  def kind 
+    if self.length_1 <= 0 || self.length_2 <= 0 || self.length_3 <= 0 
+      raise TriangleError
+    elsif self.length_1 + self.length_2 <= self.length_3 || self.length_2 + self.length_3 <= self.length_1 || self.length_1 + self.length_3 <= self.length_2
+      raise TriangleError
+    elsif self.length_1 == self.length_2 && self.length_2 == self.length_3 && self.length_1 == self.length_3
+      return :equilateral
+    elsif self.length_1 == self.length_2 || self.length_2 == self.length_3 || self.length_1 == self.length_3
+      return :isosceles
+    else
+      return :scalene 
+    end 
+  end   
+  
+  class TriangleError < StandardError
+  end   
+  
 end
