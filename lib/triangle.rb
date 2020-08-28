bc538f3102100942a8aab63ded8c0b882e030b5f
Rebecca Congi <rncongi@gmail.com>
2020-08-28 20:39:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,16 @@ class Triangle
     @length_1 = length_1
     @length_2 = length_2
     @length_3 = length_3
+    
   end 
   
   def kind 
-    
+    self.instance_variables
   end   
   
-  class TriangleError < StandardError
-  end   
+  binding.pry
+  
+  # class TriangleError < StandardError
+  # end   
   
 end
